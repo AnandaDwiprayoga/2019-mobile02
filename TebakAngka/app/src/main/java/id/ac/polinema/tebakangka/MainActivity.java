4f5b710de9a91efb25901b8080c9eb963fb467e2
AnandaDwiPrayoga <Prayogaananda04@gmail.com>
2020-02-13 22:18:19 +0700

    complete logic in started code

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -13,24 +13,43 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	private EditText etAngka;
+	private Button btnTebak;
+	private int randomNumber;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+		
+		etAngka = findViewById(R.id.number_input);
+		btnTebak= findViewById(R.id.guess_button);
+		
+		initRandomNumber();
 	}
 
 	// TODO: generate angka random di sini
 	private void initRandomNumber() {
-
+	    Random random = new Random();
+		//get random number
+		randomNumber = random.nextInt(50) + 1;
 	}
 
 	public void handleGuess(View view) {
-		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		if (Integer.parseInt(etAngka.getText().toString()) > randomNumber){
+			Toast.makeText(this, "Tebakan anda terlalu besar!", Toast.LENGTH_SHORT).show();
+		}else if (Integer.parseInt(etAngka.getText().toString()) < randomNumber){
+			Toast.makeText(this, "Tebakan anda terlalu kecil!", Toast.LENGTH_SHORT).show();
+		}else{
+			Toast.makeText(this, "Tebakan anda benar!", Toast.LENGTH_SHORT).show();
+			btnTebak.setEnabled(false);
+		}
 	}
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		initRandomNumber();
+		btnTebak.setEnabled(true);
 	}
 }
